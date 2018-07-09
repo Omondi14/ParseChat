8dd212bdc9c8ee847542cf22469ada43c3665b20
Ernest Omondi <eomondi@fb.com>
2018-07-09 11:25:32 -0700

    Milestone 2 done

---
+++ LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  ParseChat
+//
+//  Created by Ernest Omondi on 7/9/18.
+//  Copyright © 2018 Ernest Omondi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ ParseChat/LoginViewController.m
@@ -0,0 +1,109 @@
+//
+//  LoginViewController.m
+//  ParseChat
+//
+//  Created by Ernest Omondi on 7/9/18.
+//  Copyright © 2018 Ernest Omondi. All rights reserved.
+//
+
+#import "LoginViewController.h"
+#import "Parse.h"
+
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+
+@end
+
+@implementation LoginViewController
+
+- (void) showAlertWithTitle: (NSString *)title Message: (NSString *)message{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message: message
+        preferredStyle:(UIAlertControllerStyleAlert)];
+    
+    UIAlertAction *ok = [UIAlertAction actionWithTitle:@"ok" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+    }];
+    
+    [alert addAction:ok];
+    
+    [self presentViewController:alert animated:YES completion:^{
+        
+    }];
+    
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)signUpTapped:(id)sender {
+    if ([self.usernameField.text isEqual:@""]){
+        [self showAlertWithTitle:@"Username Required" Message:@"Please enter your username"];
+        NSLog(@"Username field empty");
+    }
+    else if ([self.passwordField.text isEqual:@""]){
+        [self showAlertWithTitle:@"Password Required" Message:@"Please enter your password"];
+        NSLog(@"Password field empty");
+    }
+    else{
+        [self registerUser];
+    }
+}
+- (IBAction)LoginTapped:(id)sender {
+    [self loginUser];
+}
+
+- (void)registerUser {
+    // initialize a user object
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.usernameField.text;
+    newUser.password = self.passwordField.text;
+    
+    // call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+            
+            // manually segue to logged in view
+        }
+    }];
+}
+
+- (void)loginUser {
+    NSString *username = self.usernameField.text;
+    NSString *password = self.passwordField.text;
+    
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            [self showAlertWithTitle:@"Problem Logging In" Message:@"Please try again"];
+            NSLog(@"User log in failed: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User logged in successfully");
+            
+            // display view controller that needs to shown after successful login
+        }
+    }];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
